3692e50148ffd981c168d4b7257af11331a9e931
jlopeztobar <jlopeztobar@unicauca.edu.co>
2023-09-30 22:12:08 -0500

    login update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SIRENA-Frontend'`, () => {
+  it(`should have as title 'proyectopruebas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SIRENA-Frontend');
+    expect(app.title).toEqual('proyectopruebas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('SIRENA-Frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('proyectopruebas app is running!');
   });
 });
