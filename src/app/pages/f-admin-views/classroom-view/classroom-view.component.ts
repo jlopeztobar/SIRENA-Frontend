bf83383d384656ab28b24055d0dd023bddea5232
jlopeztobar <jlopeztobar@unicauca.edu.co>
2023-11-06 23:08:57 -0500

    add classroom create

--- src/app/pages/f-admin-views/classroom-view/classroom-view.component.ts
+++ src/app/pages/f-admin-views/classroom-view/classroom-view.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router'; // Importa el Router
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'; // Importa HttpClient y HttpHeaders
+import { Router } from '@angular/router';
 import { Table } from 'src/app/models/table.model';
 
 @Component({
@@ -7,24 +8,52 @@ import { Table } from 'src/app/models/table.model';
   templateUrl: './classroom-view.component.html',
   styleUrls: ['./classroom-view.component.css']
 })
-export class ClassroomViewComponent {
+export class ClassroomViewComponent implements OnInit {
 
-  // Se debe hacer un array de salones para iterarlos
+  table_classroom: Table = {
+    title: ["ID", "Name", "Capacity", "State", "Building", "Type"],
+    li_content: [],
+    img: "https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+  };
 
-  // Aqui va la ruta que de el acceso, este solo es un ejemplo
-  route: string = "classroom_view";
+  // Inyecta HttpClient en el constructor
+  constructor(private http: HttpClient, private router: Router) { }
 
-  // Un ejemplo de como se debe usar las tablas, con el fetch setearlas
-  /*table_classroom: Table ={
-    title: ["Salon", "Capacidad", "Tipo", "Edificio", "Estado"],
-    li_content: ["201", "20", "Agua", "Fisica"],
-    state: "804000"
-  }*/
+  ngOnInit() {
+    this.fetchClassrooms();
+  }
 
-  // Inyecta el Router en el constructor
-  constructor(private router: Router) { }
+  fetchClassrooms() {
+    const token = localStorage.getItem('jwt'); // Suponiendo que se use autenticación JWT
+    if (!token) {
+      console.error('No token found!');
+      return;
+    }
 
-  // Método para manejar la navegación
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    this.http.get<any[]>('/api/v1/classroom', { headers: headers })
+      .subscribe({
+        next: (classrooms) => {
+          console.log('Classrooms data received:', classrooms);
+          // Asigna los datos de las aulas a la propiedad `li_content` del modelo
+          this.table_classroom.li_content = classrooms.map(classroom => [
+            classroom.id.toString(),
+            classroom.name,
+            classroom.capacity.toString(),
+            classroom.state,
+            classroom.building,
+            classroom.classroomType.name
+          ]);
+        },
+        error: (err) => {
+          console.error('Error fetching classrooms:', err);
+        }
+      });
+  }
+
   navigateToCreateClassroom() {
     this.router.navigate(['/admin/classroom/create']);
   }
--- src/app/pages/f-cruds/crud-classroom/crud-classroom.component.ts
+++ src/app/pages/f-cruds/crud-classroom/crud-classroom.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-crud-classroom',
   templateUrl: './crud-classroom.component.html',
   styleUrls: ['./crud-classroom.component.css']
 })
-export class CRUDClassroomComponent {
+export class CRUDClassroomComponent implements OnInit {
 
+  
+  constructor(private http: HttpClient, private router: Router) {}
+
+  private headers!: HttpHeaders; // Variable para los headers
+  public classroomTypes: any[] = [];
+
+  ngOnInit(): void {
+    this.initializeHeaders();
+    this.loadClassroomTypes();
+  }
+
+  private initializeHeaders(): void {
+    const token = localStorage.getItem('jwt');
+    if (!token) {
+      console.error('No token found!');
+      return;
+    }
+
+    this.headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
+
+  private async loadClassroomTypes() {
+    try {
+      const response2 = await this.http.get('api/v1/classroomType', { headers: this.headers }).toPromise();
+      this.classroomTypes = response2 as any[];
+      console.log(this.classroomTypes + "log");
+    } catch (error) {
+      console.error('Hubo un error al cargar los tipos de salón', error);
+      // Maneja el error adecuadamente aquí
+    }
+  }
+
+  async registerClassroom(event: Event) {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+
+    const name = (form.querySelector('#nombreSalon') as HTMLInputElement).value;
+    const capacity = Number((form.querySelector('#capacidad') as HTMLInputElement).value);
+    const state = (form.querySelector('#estadoSalon') as HTMLInputElement).value;
+    const classroomTypeSelect = form.querySelector('#tipoSalon') as HTMLSelectElement;
+    const classroomTypeId = classroomTypeSelect.selectedOptions[0].getAttribute('data-id');
+    const classroomTypeName = classroomTypeSelect.value;
+    // Asegúrate de que la propiedad "building" exista en tu formulario si es necesaria para tu objeto JSON.
+    const building = (form.querySelector('#edificio') as HTMLInputElement)?.value || '';
+    const response2 = await this.http.get('api/v1/classroomType', { headers: this.headers }).toPromise();
+
+   
+    const classroomData = {
+      name,
+      capacity,
+      state,
+      building,
+      classroomType: {
+        id: classroomTypeId ? parseInt(classroomTypeId, 10) : null,
+        name: classroomTypeName
+      },
+      userList: []
+    };
+
+    console.log('JSON que se enviará:', JSON.stringify(classroomData, null, 2));
+
+    try {
+      const response = await this.http.post('/api/v1/classroom', classroomData, { headers: this.headers }).toPromise();
+      console.log(response);
+      alert('Salón creado exitosamente');
+      this.router.navigate(['admin/home']);
+    } catch (error) {
+      console.error('Hubo un error al crear el salón', error);
+      // Aquí podrías manejar diferentes tipos de errores y dar feedback correspondiente.
+    }
+  }
 }
